721564a5d1eb4211f714d9f734cacd2cf8011dbb
Victor Paulo <victorpalo.ti@gmail.com>
2026-02-11 21:05:45 -0300

    change ci - cd

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,9 +1,19 @@
+# Isso cria o ZIP automaticamente a partir da pasta src
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../src/lambda_function.py"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
 resource "aws_lambda_function" "quicksight_lambda" {
-  filename      = "lambda_function.zip" # Você deve zipar seu código python antes
-  function_name = "get_quicksight_url"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
+  # O Terraform agora usa o arquivo gerado acima
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  
+  function_name    = "get_quicksight_url"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
 
   environment {
     variables = {
