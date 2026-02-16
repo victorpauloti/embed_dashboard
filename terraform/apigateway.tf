151da7af31d35e263a77d6bf02bf898609acfbe7
Victor Paulo <victorpalo.ti@gmail.com>
2026-02-16 16:05:54 -0300

    add authorizer api

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -14,10 +14,28 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   integration_uri  = aws_lambda_function.quicksight_lambda.invoke_arn
 }
 
+#  Cria o Autorizador do Cognito para a HTTP API
+resource "aws_apigatewayv2_authorizer" "cognito_auth" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  authorizer_type  = "JWT"
+  identity_sources = ["$request.header.Authorization"]
+  name             = "cognito-authorizer"
+
+  jwt_configuration {
+    audience = [aws_cognito_user_pool_client.client.id]
+    issuer   = "https://${aws_cognito_user_pool.pool.endpoint}"
+  }
+}
+
+#  Atualiza a Rota para usar o Autorizador
 resource "aws_apigatewayv2_route" "get_url_route" {
   api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "GET /get-url"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+
+  # Adicione estas duas linhas abaixo:
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.cognito_auth.id
 }
 
 resource "aws_apigatewayv2_stage" "default_stage" {
@@ -33,3 +51,5 @@ resource "aws_lambda_permission" "api_gw" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*/get-url"
 }
+
+
